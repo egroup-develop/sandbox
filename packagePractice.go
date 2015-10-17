63becfd6b360b39719b6548ac30908a8f1f5408c
camberbridge <camberbridgecircuit@gmail.com>
2015-10-17 20:10:28 +0900

    GETリクエスト完成

--- packagePractice.go
+++ packagePractice.go
@@ -2,26 +2,56 @@
  * このファイルは, issue#5の課題を達成するためのもの
  */
 
-
-//POST リクエスト
-
 package main
 
 import (
-    "net/http"
-    "net/url"
-    "io/ioutil"
+  "fmt"
+  "io/ioutil"
+  "net/http"
+  "net/url"
+  "time"
+)
+
+const (
+  REQ_URL = "http://weather.livedoor.com/forecast/webservice/json"
 )
 
 func main() {
+  //url.Valuesオブジェクト生成
+  values := url.Values{}
 
-    resp, _ := http.PostForm(
-        "http://weather.livedoor.com/forecast/webservice/json/v1?city=200010",
-        url.Values{"foo": {"bar"}, },
-    )
+  //パラメータ用のkey-valueを追加
+  values.Add("city", "200010")
+  fmt.Println(values.Encode())
 
-    body, _ := ioutil.ReadAll(resp.Body)
-    defer resp.Body.Close()
+  client := &http.Client{Timeout: time.Duration(10 * time.Second)}
+  get(client, values)
+}
 
-    println(string(body))
+func get(client *http.Client, values url.Values) {
+  req, err := http.NewRequest("GET", REQ_URL+"/v1", nil)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  req.URL.RawQuery = values.Encode()
+
+  resp, err := client.Do(req)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  defer resp.Body.Close()
+
+  execute(resp)
+}
+
+func execute(resp *http.Response) {
+  //response bodyを文字列で取得するサンプル
+  //ioutil.ReadAllを使う
+  b, err := ioutil.ReadAll(resp.Body)
+  if err == nil {
+    fmt.Println(string(b))
+  }
 }
