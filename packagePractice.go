fc072a72df2e6919ff42700dc46e8e6d3c641926
camberbridge <camberbridgecircuit@gmail.com>
2015-10-18 02:01:57 +0900

    GET/POST簡易版メソッドを使うことにした

--- packagePractice.go
+++ packagePractice.go
@@ -9,7 +9,9 @@ import (
   "io/ioutil"
   "net/http"
   "net/url"
+/*Duration()使う時にコメント外す
   "time"
+*/
 )
 
 const (
@@ -17,25 +19,12 @@ const (
   REQ_URL = "http://weather.livedoor.com/forecast/webservice/json"
 )
 
-//GETリクエストのレスポンスを格納用
+//GETリクエストのレスポンス格納用
 var response string = ""
 
-func main() {
-  //url.Valuesオブジェクト生成
-  values := url.Values{}
-
-  //パラメータ用のkey-valueを追加
-  values.Add("city", "200010")
-  fmt.Println(values.Encode())
-
-  client := &http.Client{Timeout: time.Duration(10 * time.Second)}
-  get(client, values)
-
-  fmt.Println(response)
-}
-
+/*時間制限ありGETを使うときコメントを外す
 func get(client *http.Client, values url.Values) {
-  req, err := http.NewRequest("GET", REQ_URL+"/v1", nil)
+  req, err := http.NewRequest("GET", REQ_URL + "/v1", nil)
   if err != nil {
     fmt.Println(err)
     return
@@ -52,13 +41,58 @@ func get(client *http.Client, values url.Values) {
 
   execute(resp)
 }
+*/
+
+
+func get(values url.Values){
+  resp, err := http.Get(REQ_URL + "/v1?" + values.Encode())
+  if err != nil{
+    fmt.Println(err)
+    return
+  }
+  defer resp.Body.Close()
+
+  execute(resp)
+}
+
+func post(values url.Values){
+  resp, err := http.PostForm("http://httpbin.org/post", values)
+  if err != nil{
+    fmt.Println(err)
+    return
+  }
+  defer resp.Body.Close()
+
+  execute(resp)
+}
 
 func execute(resp *http.Response) {
-  //response bodyを文字列で取得するサンプル
-  //ioutil.ReadAllを使う
+  //response bodyを文字列で取得
   b, err := ioutil.ReadAll(resp.Body)
   if err == nil {
     //fmt.Println(string(b))
     response = string(b)
   }
+}
+
+func main() {
+  values := url.Values{}
+
+  //リクエストパラメータ用のkey-valueを追加
+  values.Add("city", "200010")
+  fmt.Println(values.Encode())
+
+/*
+  //時間制限ありGET
+  client := &http.Client{Timeout: time.Duration(10 * time.Second)}
+  get(client, values)
+*/
+
+  //普通のGET
+  get(values)
+
+  //普通のPOST
+  post(values)
+
+  fmt.Println(response)
 }
