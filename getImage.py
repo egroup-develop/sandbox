887ac2f38ffeec41e78a93e04d8b0e94f1988291
camberbridge <camberbridgecircuit@gmail.com>
2015-11-22 19:59:48 +0900

    分類器に使う画像をロガルから取得->保存する

---
+++ getImage.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import urllib
+import urllib2
+import json
+import os
+import sys
+
+# URLを入れる
+u = ''
+title = "LogirlImages"
+
+try:
+  r = urllib2.urlopen(u)
+  
+  try: 
+    os.mkdir(title)
+    os.chdir(title)
+  except: 
+    sys.exit()
+
+  i = 0
+  for ary in json.loads(r.read()):
+    id = ary['Id']
+    urls = ary['Url']
+    num = str(i)
+
+    try:
+      os.mkdir(num + "_" + id)
+      os.chdir(num + "_" + id)
+    except: 
+      sys.exit()
+
+    print "ID: " + id
+
+    j = 1
+    for url in urls:
+      print "URL: " + url
+
+      filename = "image_" + str(j)
+      savePath = os.path.join(os.getcwd(), os.path.basename(filename))
+      urllib.urlretrieve(url, savePath)
+      j += 1
+
+    os.chdir("../")
+    i += 1
+
+
+finally: 
+  r.close()
