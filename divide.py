b5865d811f43e9f8f11f5e7799e462b74d07a1c4
camberbridge <camberbridgecircuit@gmail.com>
2015-11-24 13:15:18 +0900

    第一引数に分類してほしい画像を与えて結果としてその画像が属するクラス番号を出力. 画像の特徴量(ip2層)の出力もするようにした

--- divide.py
+++ divide.py
@@ -1,8 +1,14 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
+#本スクリプトの実行時に第一引数として特徴を抽出したい画像の相対パスを与える
 
 import caffe
 from caffe.proto import caffe_pb2
 import numpy as np
+import sys
+
+#ここではip2層の値を取り出している
+LAYER = 'ip2'
+INDEX = 0
 
 mean_blob = caffe_pb2.BlobProto()
 with open('mean.binaryproto') as f:
@@ -19,9 +25,24 @@ classifier = caffe.Classifier(
     mean=mean_array,
     raw_scale=255)
 
-#iamge...image's path
-image = caffe.io.load_image('./LogirlImages/0/image_1.png')
+#iamge...image's path(相対でよい)
+#image = caffe.io.load_image('./LogirlImages/0/image_1.png')
+image = caffe.io.load_image(sys.argv[1])
+#特徴量
 predictions = classifier.predict([image], oversample=False)
+#与えられた画像が属するクラスを表示
 pred = np.argmax(predictions)
-print(predictions)
-print(pred)
+
+#caffeで特徴抽出から分類まで完結
+print predictions
+print sys.argv[1] + "が属するクラスは, \n"
+print pred
+print "です\n"
+
+print sys.argv[1] + "の特徴量..."
+
+#caffeを特徴抽出として使う用
+#index: 素性(特徴量) の組を出力
+feat = classifier.blobs[LAYER].data[INDEX].flatten().tolist()
+for i,f in enumerate(feat): 
+  print(str(i+1) + ":" + str(f)),
